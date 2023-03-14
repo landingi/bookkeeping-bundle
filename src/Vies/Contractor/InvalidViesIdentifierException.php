8ed987b20c37b4c776635b804fafb5a9bf8a88bc
bartoszwidera <bartosz.widera@landingi.dev>
2023-03-14 09:30:42 +0100

    fix

--- src/Vies/Contractor/InvalidViesIdentifierException.php
+++ src/Vies/Contractor/InvalidViesIdentifierException.php
@@ -10,6 +10,6 @@ final class InvalidViesIdentifierException extends Exception
 {
     public static function validationFailed(string $identifier): self
     {
-        return new self(sprintf("Identifier %s is invalid", $identifier));
+        return new self(sprintf('Identifier %s is invalid', $identifier));
     }
 }
