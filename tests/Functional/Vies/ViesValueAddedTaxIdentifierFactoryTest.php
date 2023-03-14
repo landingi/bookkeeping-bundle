a6e78e2f4d1093fe873098e6d9faa47968c00da0
bartoszwidera <bartosz.widera@landingi.dev>
2023-03-14 08:05:58 +0100

    fix

--- tests/Functional/Vies/ViesValueAddedTaxIdentifierFactoryTest.php
+++ tests/Functional/Vies/ViesValueAddedTaxIdentifierFactoryTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace Landingi\BookkeepingBundle\Functional\Vies;
 
 use DragonBe\Vies\Vies;
+use Exception;
+use Landingi\BookkeepingBundle\Bookkeeping\Contractor\Company\ValueAddedTax\SimpleIdentifier;
+use Landingi\BookkeepingBundle\Bookkeeping\Contractor\Company\ValueAddedTax\ValidatedIdentifier;
 use Landingi\BookkeepingBundle\Vies\Contractor\Company\ValueAddedTax\ViesIdentifierFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -14,6 +17,7 @@ final class ViesValueAddedTaxIdentifierFactoryTest extends TestCase
         $factory = new ViesIdentifierFactory(new Vies());
         $identifier = $factory->create('29480969591', 'FR');
 
+        self::assertTrue($identifier instanceof ValidatedIdentifier);
         self::assertEquals('FR29480969591', $identifier->toString());
     }
 
@@ -22,6 +26,15 @@ final class ViesValueAddedTaxIdentifierFactoryTest extends TestCase
         $factory = new ViesIdentifierFactory(new Vies());
         $identifier = $factory->create('6762461659', 'PL');
 
+        self::assertTrue($identifier instanceof SimpleIdentifier);
         self::assertEquals('6762461659', $identifier->toString());
+    }
+
+    public function testItIsInvalidIdentifier(): void
+    {
+        $factory = new ViesIdentifierFactory(new Vies());
+        $factory->create('333111222', 'DE');
+
+        self::expectException(Exception::class);
     }
 }
