5431dd4af1c6e5b983e8b8994b1e90208a47a87a
pwitko <piotr@landingi.com>
2023-05-15 13:17:35 +0200

    Sigh

--- tests/Integration/Wfirma/Invoice/WfirmaInvoiceBookTest.php
+++ tests/Integration/Wfirma/Invoice/WfirmaInvoiceBookTest.php
@@ -136,8 +136,7 @@ final class WfirmaInvoiceBookTest extends IntegrationTestCase
         $invoices = $this->invoiceBook->list(1, ...$conditions);
         $this->assertEmpty(array_filter(
             $invoices->getAll(),
-            fn(Invoice $filterCandidate) =>
-                (string) $filterCandidate->getFullNumber() === (string) $invoice->getFullNumber()
+            fn(Invoice $result) => (string) $result->getFullNumber() === (string) $invoice->getFullNumber()
         ));
 
         //test delete
