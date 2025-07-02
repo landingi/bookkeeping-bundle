bcad273e0e66ef41ce29534f85384e73a6c84180
bartoszwidera <bartosz.widera@landingi.dev>
2025-07-02 13:19:51 +0200

    [PLT-1693]Use curl request instead SOAP client in Vat validation

---
+++ src/Vies/Client/ViesClient.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Landingi\BookkeepingBundle\Vies\Client;
+
+use Landingi\BookkeepingBundle\Curl\Curl;
+use Landingi\BookkeepingBundle\Curl\CurlException;
+use function json_decode;
+use function sprintf;
+
+final class ViesClient
+{
+    private const VIES_PROTOCOL = 'https';
+    private const VIES_DOMAIN = 'ec.europa.eu';
+    private const VIES_PATH = 'taxation_customs/vies/rest-api/ms/%s/vat/%s';
+
+
+    public function validateVat(string $countryCode, string $identifier): array
+    {
+        $curl = $this->getCurl($countryCode, $identifier);
+        $response = json_decode($curl->requestGET(), true, 512, JSON_THROW_ON_ERROR);
+
+        if (!isset($response['isValid'])) {
+            throw new \RuntimeException('Invalid response from VIES REST API');
+        }
+
+        return $response;
+    }
+
+    /**
+     * @throws CurlException
+     */
+    private function getCurl(string $countryCode, string $identifier): Curl
+    {
+        $path = sprintf(
+            self::VIES_PATH,
+            $countryCode,
+            $identifier
+        );
+
+        return Curl::withHeaderAuth(
+            sprintf(
+                '%s://%s/%s',
+                self::VIES_PROTOCOL,
+                self::VIES_DOMAIN,
+                $path,
+            ),
+            []
+        );
+    }
+}
--- tests/Integration/Vies/ViesValueAddedTaxIdentifierFactoryTest.php
+++ tests/Integration/Vies/ViesValueAddedTaxIdentifierFactoryTest.php
@@ -3,11 +3,11 @@ declare(strict_types=1);
 
 namespace Landingi\BookkeepingBundle\Integration\Vies;
 
-use DragonBe\Vies\Vies;
 use Landingi\BookkeepingBundle\Bookkeeping\Contractor\Company\ValueAddedTax\SimpleIdentifier;
 use Landingi\BookkeepingBundle\Bookkeeping\Contractor\Company\ValueAddedTax\ValidatedIdentifier;
 use Landingi\BookkeepingBundle\Integration\IntegrationTestCase;
 use Landingi\BookkeepingBundle\Unit\Fake\ThrowingVies;
+use Landingi\BookkeepingBundle\Vies\Client\ViesClient;
 use Landingi\BookkeepingBundle\Vies\Contractor\Company\ValueAddedTax\ViesIdentifierFactory;
 use Landingi\BookkeepingBundle\Vies\Contractor\InvalidViesIdentifierException;
 use Landingi\BookkeepingBundle\Vies\Exception\ViesServiceException;
@@ -17,46 +17,35 @@ final class ViesValueAddedTaxIdentifierFactoryTest extends IntegrationTestCase
 {
     public function testItIsValidIdentifier(): void
     {
-        $factory = new ViesIdentifierFactory(new Vies());
+        $factory = new ViesIdentifierFactory(new ViesClient());
         $identifier = $factory->create('29480969591', 'FR');
 
         self::assertTrue($identifier instanceof ValidatedIdentifier);
         self::assertEquals('FR29480969591', $identifier->toString());
-        sleep(1); // Sleeps added because of VIES API rate limit.
     }
 
     public function testItIsValidPolishIdentifier(): void
     {
-        $factory = new ViesIdentifierFactory(new Vies());
+        $factory = new ViesIdentifierFactory(new ViesClient());
         $identifier = $factory->create('6762461659', 'PL');
 
         self::assertTrue($identifier instanceof SimpleIdentifier);
         self::assertEquals('6762461659', $identifier->toString());
-        sleep(1);
     }
 
     public function testItIsInvalidIdentifier(): void
     {
         self::expectException(InvalidViesIdentifierException::class);
 
-        $factory = new ViesIdentifierFactory(new Vies());
+        $factory = new ViesIdentifierFactory(new ViesClient());
         $factory->create('333111222', 'DE');
-        sleep(1);
     }
 
     public function testItThrowsWhenViesThrowsGenericException(): void
     {
-        self::expectException(ViesException::class);
+        self::expectException(InvalidViesIdentifierException::class);
 
-        $factory = new ViesIdentifierFactory(ThrowingVies::viesException());
-        $factory->create('333111222', 'DE');
-    }
-
-    public function testItThrowsWhenViesThrowsServiceException(): void
-    {
-        self::expectException(ViesServiceException::class);
-
-        $factory = new ViesIdentifierFactory(ThrowingVies::serviceException());
+        $factory = new ViesIdentifierFactory(new ViesClient());
         $factory->create('333111222', 'DE');
     }
 }
